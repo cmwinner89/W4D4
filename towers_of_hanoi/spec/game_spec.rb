4e5a3bb87b9e76f6458ec311186d473ecb4829d8
fdiengott <freddiengott@gmail.com>
2021-03-18 15:46:57 -0400

    Initial commit with towers of hanoi skeleton

---
+++ towers_of_hanoi/spec/game_spec.rb
@@ -0,0 +1,23 @@
+# 3 'disc' arrays
+# 1, 2, 3
+# disc class with one attribute, @size
+
+#move
+# should move the top disc of a stack to one of the other stacks
+  # only one
+# should not move a disc onto a smaller disc
+
+#won?
+# should last peg has all discs in correct order [1,2,3]
+
+require 'game'
+
+describe Game do 
+  
+
+
+
+
+
+
+end
