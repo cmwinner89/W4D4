5d3843ef0dba60fdfec1503ce74d8dc12237a159
fdiengott <freddiengott@gmail.com>
2021-03-18 16:25:44 -0400

    implement Game#move specs

--- towers_of_hanoi/spec/game_spec.rb
+++ towers_of_hanoi/spec/game_spec.rb
@@ -13,11 +13,45 @@
 require 'game'
 
 describe Game do 
+  subject(:game) { Game.new }
+
+  let(:stack1) { [3,2,1] }
+  let(:stack2) { [] }
+  let(:stack3) { [] }
+
+  describe "#move" do 
+    it "should remove from the end of the stack" do 
+      game.move
+      expect(stack1).to eq([3,2])
+    end
+
+    it "should move a disc to another stack" do 
+      game.move
+      expect(stack2).to eq([1]).or expect(stack3).to eq([1])
+    end
+
+    it "should not place a larger disc on a smaller disc" do 
+      stack1 = [3]
+      stack2 = [1]
+      stack3 = [2]
+      game.move
+      
+      expect(stack2).not_to eq([1,3])
+      expect(stack3).not_to eq([2,3])
+    end
+  end
+
+  describe "#won?" do 
+
+  end
+
+
+
+
   
-
-
-
-
-
-
 end
+  
+  # allow(double).to receive(:method).and_return(some_value)
+  
+  # let(:customer) { double("customer", :email_address => "ned@appacademy.io") }
+
