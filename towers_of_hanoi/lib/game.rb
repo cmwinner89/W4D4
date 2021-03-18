8cfd675b9c7db2962b29e85b9e2814ef66067dc7
Collin Winner <cmwinner89@gmail.com>
2021-03-18 17:20:14 -0400

    Create initialize and move methods

--- towers_of_hanoi/lib/game.rb
+++ towers_of_hanoi/lib/game.rb
@@ -5,12 +5,19 @@ class Game
     @stack1 = [3,2,1]  
     @stack2 = []  
     @stack3 = []  
+    @hash = {0 => @stack1, 1 => @stack2, 2 => @stack3}
+    
   end
 
   def move(from_stack, to_stack)
-    to_stack << from_stack.pop 
-
+    if !hash[to_stack].empty?
+      if hash[from_stack].last > hash[to_stack].last  
+        raise "Invalid move"
+      end
+      hash[to_stack] << hash[from_stack].pop 
+    end
   end
 
-
+  private
+  attr_reader :hash
 end
